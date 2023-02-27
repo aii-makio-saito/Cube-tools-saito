834cdc099da4712a79a153c0206d3a6eb3975a49
aii-makio-saito <106638558+aii-makio-saito@users.noreply.github.com>
2023-02-27 13:47:38 +0900

    修正

--- setWireguardConf.sh
+++ setWireguardConf.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-file=/etc/netplan/99-remote-server.yaml
-bkfile=/etc/netplan/99-remote-server.yaml.bak
-
 # 引数チェック
 if [ $# -ne 1 ]; then
-  echo "usage: $0 [new address]"
-  exit 1
+    echo "Usage: $0 [new_address]"
+    exit 1
 fi
 
-# ファイルバックアップ作成
-cp $file $bkfile
+# 対象ファイルのパス
+file_path="/Users/saitoumakio/Documents/GitHub/Cube-tools-saito/99-remote-server.yaml"
 
-# 変換処理
-sed -i -e '/^ *enx04ab18f8625c:/,/^ *nameservers:/ s/^ *#*/    /; /^ *enx04ab18f8625c:/a\
-      addresses: '"$1"'\
-' "$file"
-sed -i -e '8d' $file
-sed -i -e '/gateway4:/s/^/#/' $file
-sed -i -e '8d' $file
+# gateway4コメント化の制御
+if grep -q "^[[:blank:]]*#*gateway4:" "$file_path"; then
+    if grep -q "^[[:blank:]]*gateway4:" "$file_path"; then
+        sed -i'.bak' 's/gateway4:/#&/' "$file_path"
+    fi
+else
+    if grep -q "^[[:blank:]]*gateway4:" "$file_path"; then
+        sed -i'.bak' 's/#*gateway4:/&/' "$file_path"
+    else
+        sed -i'.bak' '8 a #gateway4: ' "$file_path"
+    fi
+fi
 
-echo "${file}のファイルを書き換えました。"
-
-cat $file
+# 8行目のaddressesを変更する
+if grep -q "^[[:blank:]]*addresses:" "$file_path"; then
+    sed -i'.bak' "8 s|.*|        - $1|" "$file_path"
+fi
+rm "${file_path}.bak"
